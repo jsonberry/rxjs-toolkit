9728d054bdcfb83b831d96615a94f71551931460
jsonberry <jason.s.awbrey@gmail.com>
2018-11-22 07:15:20 -0800

    test(pick): add initial AVA tests for pick

--- src/lib/pick.spec.ts
+++ src/lib/pick.spec.ts
@@ -1,5 +1,31 @@
 import test from 'ava';
+import { of } from 'rxjs';
+import { mapTo, tap } from 'rxjs/operators';
+import { pick } from './pick';
 
-test('tests to come, just getting this off the ground for now', t => {
-  t.pass();
+const fixture = { foo: 'foo', bar: 'bar', baz: 'baz', orb: { rob: 'bro' } };
+
+test('pick should pick one or more properties from an object', t => {
+  return of(fixture).pipe(
+    pick('foo'),
+    tap(x => t.deepEqual(x, { foo: 'foo' }))
+  );
+});
+
+test('pick should get multiple properties', t => {
+  return of(fixture).pipe(
+    pick('bar', 'baz'),
+    tap(x => t.deepEqual(x, { bar: 'bar', baz: 'baz' }))
+  );
+});
+
+test('pick should return an empty object when the options passed are not valid', t => {
+  return of(fixture).pipe(
+    pick('zab'),
+    tap(x => t.deepEqual(x, {})),
+    mapTo(fixture),
+
+    pick('orb.rob.bro'),
+    tap(x => t.deepEqual(x, {}))
+  );
 });
