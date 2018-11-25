9471c74e5325e3e59443edf270218217bc954123
jsonberry <jason.s.awbrey@gmail.com>
2018-11-25 09:42:47 -0800

    feat: add hasProps

---
+++ src/lib/has-props.ts
@@ -0,0 +1,31 @@
+import _has from 'lodash.has';
+import { Observable, of, throwError } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+export function hasPropsGuard<T>(
+  signal: T,
+  args: Array<keyof T>
+): signal is Required<T> {
+  for (const arg of args) {
+    if (!_has(signal, arg)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function hasProps<T>(...args: Array<keyof T>) {
+  return (source$: Observable<T>) =>
+    source$.pipe(
+      mergeMap((signal: T) =>
+        hasPropsGuard(signal, args)
+          ? of(signal)
+          : throwError(
+              new Error(
+                'Signal was missing required property. Check the interface of the signal for the properties being asked for.'
+              )
+            )
+      )
+    );
+}
