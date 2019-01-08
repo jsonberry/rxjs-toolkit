9c8dbdc62ef6a93d6f9592c29eb4569ce3061c66
jsonberry <jason.s.awbrey@gmail.com>
2019-01-08 15:10:55 -0800

    fix(propsAreTruthy): add propsAreTruthy to barrel file

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './lib/has-props';
 export * from './lib/ignore-falsy-signals';
 export * from './lib/never-nullable';
 export * from './lib/pick';
+export * from './lib/props-are-truthy';
 export * from './lib/tap-log';
