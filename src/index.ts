f484061cc9615f0a840c8eb7c46e72728906f42f
jsonberry <jason.s.awbrey@gmail.com>
2019-01-16 13:08:28 -0800

    feat: add mapToDictionary

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from './lib/ignore-falsy-signals';
 export * from './lib/never-nullable';
 export * from './lib/pick';
 export * from './lib/props-are-truthy';
-export * from './lib/tap-log';
+export * from './lib/operators';
